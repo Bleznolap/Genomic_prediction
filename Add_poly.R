be69f26248bd9e1161f7c1f79f847768d627e25b
Blessing Olabosoye <83044217+Bleznolap@users.noreply.github.com>
2024-05-24 13:43:58 -0500

    Update Add_poly.R

--- Add_poly.R
+++ Add_poly.R
@@ -1,4 +1,10 @@
 
+#' @param pheno A phenotype dataframe output of filter.R function for the target population
+#' @param ... A list of polygenic scores computed using Get_poly.R for each source population
+#' @returns a phenotype dataframe containing the polygenic score(s) 
+
+#' @details
+#' This function adds the polygenic score(s) to the target population phenotype data
 
 #Add poly
 
