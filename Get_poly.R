490e212f0c828d3a16b632e184495c99eb66aced
Blessing Olabosoye <83044217+Bleznolap@users.noreply.github.com>
2024-05-24 13:38:32 -0500

    Update Get_poly.R

--- Get_poly.R
+++ Get_poly.R
@@ -1,4 +1,14 @@
 
+#' @param g A genotype matrix n X m output of filter.R function for the target population
+#' @param gwa A gwas object from gwa.R for the source population
+#' @param scale A condition to standardize the genotype matrix (true by default)
+#' @returns a matrix n x 1
+
+#' @details
+#' This function computes the polygenic score using the genotype matrix of the source population (receiving 
+#' population) and the SNP effects of the target population (s) (contributing population(s)). If scale is FALSE,
+#' the standardized genotype matrix (Z matrix) can replace genotype matrix.
+
 #Get poly
 
 get_polys<- function(g, gwa, scale=TRUE){
